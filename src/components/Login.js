4ed9d7f4172ff8d41b0e5e97daee6b363a1140a2
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-02 20:12:09 +0530

    fixed: password field bug fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -68,6 +68,7 @@ const Login = () => {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
               placeholder="Enter Password..."
               value={credentials.password || ""}
               onChange={onChange}
