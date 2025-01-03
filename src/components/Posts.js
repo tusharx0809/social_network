d5df95f8e9a534f41df65faab619768eec78f23e
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-03 18:50:38 +0530

    update: font-weight-changed in profile and posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -295,7 +295,7 @@ const Posts = () => {
   return (
     <div>
       <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+        <h5 className="mb-2 text-3xl font-light text-gray-900 dark:text-white">
           Latest Posts
         </h5>
         <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React,{useContext, } from 'react'
+import profileContext from "../context/profile/ProfileContext";
 
 const Profile = () => {
+    const { alert, user, showAlert, getUserProfile } = useContext(profileContext);
   return (
     <div>
-      This is profile
+       <>
+    <div>
+        {/* Display alert if exists */}
+        {alert && alert.message && alert.type && (
+          <div className={`p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 ${
+            alert.type === "success" 
+            ? "dark:text-green-400"
+            : alert.type === "danger"
+            ? "dark:text-red-400"
+            : ""
+          }`}
+          style={{
+            position: "fixed",
+            top: "70px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 10,
+            width: "500px",
+            padding: "10px",
+            textAlign: "center",
+            borderRadius: "20px",
+          }}>
+          <span className="font-medium">Alert: </span> {alert.message}
+        </div>
+        )}
+      </div>
+    <div className="container mx-auto my-10 p-4 max-w-4xl">
+    <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <p className="mb-2 text-3xl font-light text-gray-900 dark:text-white">
+          User Profile
+        </p>
+        
+
+        
+        
+      </div>
+    </div>
+    </>
     </div>
   )
 }
