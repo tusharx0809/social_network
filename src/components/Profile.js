09e4a12fa76e8e8210988ed038f18ccba97fb012
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-03 19:38:28 +0530

    updated: friends component added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -99,7 +99,7 @@ const Profile = () => {
             </h5>
             <div className="flex justify-center items-center">
               <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <div className="flow-root">
+                <div className="flow-root" style={{maxHeight:"300px", overflowY:"scroll"}}>
                   <ul role="list" className="list-inside space-y-3 text-center">
                     {user &&
                       user.friends &&
@@ -110,7 +110,7 @@ const Profile = () => {
                               {friend.name}&nbsp;
                             </p>
                             <button
-                              className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-3"
+                              className="text-white bg-red-300 hover:bg-red-300 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-500 dark:hover:bg-red-500 dark:focus:ring-red-900 mt-3"
                               onClick={() => removeFriend(friend._id)}
                             >
                               Remove
@@ -131,17 +131,30 @@ const Profile = () => {
             </h5>
             <div className="flex justify-center items-center">
               <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <div className="flow-root">
+                <div className="flow-root" style={{maxHeight:"300px", overflowY:"scroll"}}>
                   <ul role="list" className="list-inside space-y-3 text-center">
                     {user &&
-                    user.friendsRequests &&
-                    user.friendsRequests.length > 0 ? (
-                      user.friendsRequests.map((friend, index) => (
+                    user.friendRequests &&
+                    user.friendRequests.length > 0 ? (
+                      user.friendRequests.map((friend, index) => (
                         <li key={friend._id || index}>
-                          <div className="flex flex-row items-center">
+                          <div className="flex flex-row items-center justify-between">
                             <p className="text-sm font-medium text-gray-900 dark:text-white">
                               {friend.name}&nbsp;
                             </p>
+                            <div>
+                            <button
+                                  className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                  onClick={() => acceptRequest(friend._id)}
+                                >
+                                  Accept
+                                </button>
+                                <button
+                                  className="text-white bg-red-300 hover:bg-red-300 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-500 dark:hover:bg-red-500 dark:focus:ring-red-900"
+                                  onClick={() => rejectRequest(friend._id)}
+                                >
+                                  Reject
+                                </button></div>
                           </div>
                         </li>
                       ))
